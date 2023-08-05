36ac87d8acbc5277efa2de7a7ffe9957a4d83f73
unknown <ccao2776@gmail.com>
2023-08-04 21:02:34 -0500

    support AWS lambda func

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM public.ecr.aws/lambda/python@sha256:10dbb67ede15b5fd516be87dd71c3f7968904b0b840235720486476b34ef9b67 as build
+RUN yum install -y unzip && \
+    curl -Lo "/tmp/chromedriver.zip" "https://chromedriver.storage.googleapis.com/114.0.5735.90/chromedriver_linux64.zip" && \
+    curl -Lo "/tmp/chrome-linux.zip" "https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2F1135561%2Fchrome-linux.zip?alt=media" && \
+    unzip /tmp/chromedriver.zip -d /opt/ && \
+    unzip /tmp/chrome-linux.zip -d /opt/
+
+FROM public.ecr.aws/lambda/python@sha256:10dbb67ede15b5fd516be87dd71c3f7968904b0b840235720486476b34ef9b67
+RUN yum install atk cups-libs gtk3 libXcomposite alsa-lib \
+    libXcursor libXdamage libXext libXi libXrandr libXScrnSaver \
+    libXtst pango at-spi2-atk libXt xorg-x11-server-Xvfb \
+    xorg-x11-xauth dbus-glib dbus-glib-devel -y
+RUN pip install selenium==4.9.1
+RUN pip install requests==2.30.0
+COPY --from=build /opt/chrome-linux /opt/chrome
+COPY --from=build /opt/chromedriver /opt/
+COPY /panda-express/main.py ./
+CMD [ "main.handler" ]
--- Panda-Express/main.py
+++ Panda-Express/main.py
@@ -1,38 +1,63 @@
 import re
 import requests
-
+from tempfile import mkdtemp
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
-def main():
-
-    scraper = webdriver.Chrome()
-    scraper.get('https://www.pandaguestexperience.com/')
-    button = scraper.find_element(By.ID, "NextButton")
+def handler(event=None, context=None):
+    options = webdriver.ChromeOptions()
+    options.binary_location = '/opt/chrome/chrome'
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1280x1696")
+    options.add_argument("--single-process")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-dev-tools")
+    options.add_argument("--no-zygote")
+    options.add_argument(f"--user-data-dir={mkdtemp()}")
+    options.add_argument(f"--data-path={mkdtemp()}")
+    options.add_argument(f"--disk-cache-dir={mkdtemp()}")
+    options.add_argument("--remote-debugging-port=9222")
+    scraper = webdriver.Chrome("/opt/chromedriver",
+                              options=options)
 
     # navigation
+    scraper.get('https://www.pandaguestexperience.com/')
+    button = scraper.find_element(By.ID, "NextButton")
     while button and ("100" not in scraper.title):
         button.click()
         try:
-            button = WebDriverWait(scraper, 10).until(
+            button = WebDriverWait(scraper, 3).until(
                 EC.presence_of_element_located((By.ID, "NextButton"))
             )
         except:
+           scraper.close()
            scraper.quit()
+           return {"statusCode": 100, "body": "FAIL"}
     
-    # fill in form using generated email
+    # # # fill in form using generated email
     addr = generate_email()
-    scraper.find_element(By.XPATH, "//input[@id='S000057']").send_keys(self._addr)
-    scraper.find_element(By.XPATH, "//input[@id='S000064']").send_keys(self._addr)
+    scraper.find_element(By.XPATH, "//input[@id='S000057']").send_keys(addr)
+    scraper.find_element(By.XPATH, "//input[@id='S000064']").send_keys(addr)
     scraper.find_element(By.ID, "NextButton").click()
 
     # retrive discount code from inbox
     login, domain = addr.split("@")[0], addr.split("@")[1]
 
     code_html = get_code(login, domain)
-    parse_code(code_html)
+    code = parse_code(code_html)
+
+    scraper.close()
+    scraper.quit()
+    response = {
+        "statusCode": 200,
+        "body": code
+    }
+
+    return response
 
 def generate_email():
     URL = 'https://www.1secmail.com/api/v1/'
@@ -46,10 +71,11 @@ def get_code(login, domain):
     payload = {'action': 'getMessages', 'login': login, 'domain': domain}
 
     while True:
-        message = requests.get(URL, payload).json()[0]
+        message = requests.get(URL, payload).json()
         if message:
             break
     
+    message = message[0]
     # scan individual messages for codes (in HTML format)
     payload['action'] = 'readMessage'
     id = message['id']
@@ -63,8 +89,9 @@ def parse_code(code_html):
 
     match = re.findall(pattern, code_html)
     if match:
-        print("Code:", match)
-
+        return match
+    
+    return None
 
 if __name__ == '__main__':
     main()
